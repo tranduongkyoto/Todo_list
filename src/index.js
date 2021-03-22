a9cc489bc4ca55c5f0af637ad078afc0be76f521
Dương Trần <tranduongkyoto@gmail.com>
2021-03-22 17:56:11 +0700

    setting redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import { applyMiddleware, createStore, compose} from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import appReducers from './reducers/index';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+const composeEnhancers = composeWithDevTools({
 
-const store = createStore(
-    appReducers,
-    compose(
-        applyMiddleware(thunk), 
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+});
+const store = createStore(appReducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunk),
+));
+// const store = createStore(
+//     appReducers,
+//     compose(
+//         applyMiddleware(thunk),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// );
 
 ReactDOM.render(
     <Provider store={store}>
@@ -19,4 +25,4 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
-//registerServiceWorker();
+
