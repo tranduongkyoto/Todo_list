947583a95447c584380f85e74f8389165bae83cc
Dương Trần <tranduongkyoto@gmail.com>
2021-03-22 21:21:31 +0700

    redux

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(
     appReducers,
     compose(
         applyMiddleware(thunk),
-
+	    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
